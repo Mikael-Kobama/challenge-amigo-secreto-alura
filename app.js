0eb6404e8414661402e871706e5e1f70f70bbdee
DevKobama <125419153+Mikael-Kobama@users.noreply.github.com>
2025-01-21 13:37:53 -0300

    Update app.js

--- app.js
+++ app.js
@@ -19,4 +19,22 @@ function exibirAmigos() {
   const listaAmigos = document.getElementById("listaAmigos");
 
   listaAmigos.value = "";
+
+  for(let amigo of amigos) {
+    const li = document.createElement("li")
+    li.textContent = amigo;
+    const listaAmigos = document.getElementById("listaAmigos");
+  }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Por favor, adicione pelo menos um amigo para sortear.");
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+    const amigoSorteado = amigos[indiceAleatorio];
+    document.getElementById("resultado").innerHTML = `Amigo Sorteado: ${amigoSorteado}`;
 }
