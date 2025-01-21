b26228e6669520c19a937d7f887ea35de563714e
DevKobama <125419153+Mikael-Kobama@users.noreply.github.com>
2025-01-21 05:15:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+let amigos = [];
 
+function adicionarAmigo() {
+  const nomeAmigo = document.getElementById("amigo").value;
+
+  if(nomeAmigo.trim() === "") {
+    alert("Por favor, insira um nome.");
+        return;
+  }
+
+  amigos.push(nomeAmigo);
+
+  document.getElementById("amigo").value = "";
+  exibirAmigos();
+}
+
+
+function exibirAmigos() {
+  const listaAmigos = document.getElementById("listaAmigos");
+
+  listaAmigos.value = "";
+}
